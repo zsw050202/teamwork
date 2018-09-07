816e0e6b54a24545c947f6b59a8b846e1b40fe88
zsw <1050259219@qq.com>
2018-09-07 21:53:56 +0800

    public fun updata

--- pulic.js
+++ pulic.js
@@ -3,5 +3,8 @@ function pulic () {
 	console.log('classtow console');
 }
 function public2 () {
-	console.log('classOne console2');
+	console.log('2222');
+}
+function publick () {
+	console.log('1111');
 }
